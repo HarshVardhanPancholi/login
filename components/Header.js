b07b60214cf387163f29423dc4f8e62f997149a6
Harsh Vardhan Pancholi <36657761+HarshVardhanPancholi@users.noreply.github.com>
2022-01-22 14:10:30 +0530

    Add files via upload

---
+++ components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Header()
+{
+    return(
+        <>
+            <nav className="container-fluid navbar navbar-dark p-2 text-center" style={{backgroundImage: "linear-gradient(15deg, #5ab9ea 0%,#e7e3d4 100%)"}}>
+            <span className="navbar-brand text-uppercase mb-0 h1">Discover Training Centre</span>
+            </nav>
+        
+        </>
+        
+    )
+}
+
+export default Header;
